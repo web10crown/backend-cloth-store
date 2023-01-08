ef3598fcbd28cc8b5726493c02469e6da40e52cb
web10crown <99588906+web10crown@users.noreply.github.com>
2023-01-08 15:39:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const multer = require("multer");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors("https://crown-cloth-store.onrender.com/"));
 
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
